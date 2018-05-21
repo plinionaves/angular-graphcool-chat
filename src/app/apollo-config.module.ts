f69a2689ebe1e230661976ae63a4e4637230eb85
Plínio Naves <pliniopjn@hotmail.com>
2018-05-21 16:30:48 -0300

    Secao 04 - Manipulando erros com Apollo Link Error

--- src/app/apollo-config.module.ts
+++ src/app/apollo-config.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
 import { Apollo, ApolloModule } from 'apollo-angular';
+import { ApolloLink } from 'apollo-link';
 import { HttpLink, HttpLinkModule } from 'apollo-angular-link-http';
+import { onError } from 'apollo-link-error';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { environment } from '../environments/environment';
 
@@ -23,8 +25,23 @@ export class ApolloConfigModule {
     const uri = 'https://api.graph.cool/simple/v1/cjgzdlduy0d100166nmfdz1qr';
     const http = httpLink.create({ uri });
 
+    const linkError = onError(({ graphQLErrors, networkError }) => {
+      if (graphQLErrors) {
+        graphQLErrors.map(({ message, locations, path }) =>
+          console.log(
+            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+          ),
+        );
+      }
+
+      if (networkError) { console.log(`[Network error]: ${networkError}`); }
+    });
+
     apollo.create({
-      link: http,
+      link: ApolloLink.from([
+        linkError,
+        http
+      ]),
       cache: new InMemoryCache(),
       connectToDevTools: !environment.production
     });
