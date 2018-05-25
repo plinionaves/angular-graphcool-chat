ffb4326538bd4d771b73521b4b70681bc8ab71fa
Plínio Naves <pliniopjn@hotmail.com>
2018-05-24 21:43:56 -0300

    Secao 06 - Implementando authenticateUser

--- graphcool/src/email-password/authenticate.ts
+++ graphcool/src/email-password/authenticate.ts
@@ -1,52 +1,53 @@
-import { fromEvent, FunctionEvent } from 'graphcool-lib'
-import { GraphQLClient } from 'graphql-request'
-import * as bcrypt from 'bcryptjs'
+import { default as Graphcool, fromEvent, FunctionEvent } from 'graphcool-lib';
+import { GraphQLClient } from 'graphql-request';
+import * as bcrypt from 'bcryptjs';
 
 interface User {
-  id: string
-  password: string
+  id: string;
+  password: string;
 }
 
 interface EventData {
-  email: string
-  password: string
+  email: string;
+  password: string;
 }
 
-const SALT_ROUNDS = 10
+const SALT_ROUNDS = 10;
 
 export default async (event: FunctionEvent<EventData>) => {
-  console.log(event)
+  console.log(event);
 
   try {
-    const graphcool = fromEvent(event)
-    const api = graphcool.api('simple/v1')
+    const graphcool: Graphcool = fromEvent<EventData>(event);
+    const api: GraphQLClient = graphcool.api('simple/v1');
 
-    const { email, password } = event.data
+    const { email, password } = event.data;
 
     // get user by email
     const user: User = await getUserByEmail(api, email)
-      .then(r => r.User)
+      .then(r => r.User);
 
     // no user with this email
     if (!user) {
-      return { error: 'Invalid credentials!' }
+      return { error: 'Invalid credentials!' };
     }
 
     // check password
-    const passwordIsCorrect = await bcrypt.compare(password, user.password)
+    const passwordIsCorrect = await bcrypt.compare(password, user.password);
     if (!passwordIsCorrect) {
-      return { error: 'Invalid credentials!' }
+      return { error: 'Invalid credentials!' };
     }
 
     // generate node token for existing User node
-    const token = await graphcool.generateNodeToken(user.id, 'User')
+    const token = await graphcool.generateNodeToken(user.id, 'User');
 
-    return { data: { id: user.id, token} }
+    return { data: { id: user.id, token} };
+
   } catch (e) {
-    console.log(e)
-    return { error: 'An unexpected error occured during authentication.' }
+    console.log(e);
+    return { error: 'An unexpected error occured during authentication.' };
   }
-}
+};
 
 async function getUserByEmail(api: GraphQLClient, email: string): Promise<{ User }> {
   const query = `
@@ -56,11 +57,11 @@ async function getUserByEmail(api: GraphQLClient, email: string): Promise<{ User
         password
       }
     }
-  `
+  `;
 
   const variables = {
     email,
-  }
+  };
 
-  return api.request<{ User }>(query, variables)
+  return api.request<{ User }>(query, variables);
 }
