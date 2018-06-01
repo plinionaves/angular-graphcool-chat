f1f7a1ad1c6c6432305fa88aa0867ec40f98d287
Plínio Naves <pliniopjn@hotmail.com>
2018-06-01 14:41:04 -0300

    Secao 07 - AuthGuard para rotas protegidas: CanActivateChild

--- src/app/login/auth.guard.ts
+++ src/app/login/auth.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, CanActivateChild, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs';
 
 import { AuthService } from '../core/services/auth.service';
@@ -8,7 +8,7 @@ import { LoginRoutingModule } from './login-routing.module';
 @Injectable({
   providedIn: LoginRoutingModule
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanActivateChild {
 
   constructor(
     private authService: AuthService
@@ -20,4 +20,12 @@ export class AuthGuard implements CanActivate {
   ): Observable<boolean> {
     return this.authService.isAuthenticated;
   }
+
+  canActivateChild(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean> {
+    return this.canActivate(route, state);
+  }
+
 }
