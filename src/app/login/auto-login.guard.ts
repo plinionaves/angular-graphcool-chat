b06b25069b7ce38299023751bd0c15bf29b98c3d
Plínio Naves <pliniopjn@hotmail.com>
2018-06-04 17:55:03 -0300

    Secao 07 - AutoLoginGuard com CanActivate para rota de login

---
+++ src/app/login/auto-login.guard.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
+import { Observable, of } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
+
+import { AuthService } from '../core/services/auth.service';
+
+@Injectable()
+export class AutoLoginGuard implements CanActivate {
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean> {
+    /*return this.authService.isAuthenticated
+      .pipe(
+        tap(is => (is) ? this.router.navigate(['/dashboard']) : null),
+        map(is => !is)
+      );*/
+    return of(true);
+  }
+}
--- src/app/login/login-routing.module.ts
+++ src/app/login/login-routing.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { AutoLoginGuard } from './auto-login.guard';
 import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent, canActivate: [ AutoLoginGuard ] }
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [ AutoLoginGuard ]
 })
 export class LoginRoutingModule { }
