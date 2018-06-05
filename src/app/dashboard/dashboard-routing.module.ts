73ce5bf1a91fab47b3d2d1568d93eb366c7029c3
Plínio Naves <pliniopjn@hotmail.com>
2018-06-05 15:11:40 -0300

    Secao 08 - Configurando rota interna para Component Home

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { DashboardHomeComponent } from './components/dashboard-home/dashboard-home.component';
+
+const routes: Routes = [
+  { path: '', component: DashboardHomeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
