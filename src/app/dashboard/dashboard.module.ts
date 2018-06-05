8846461178dfd78e1720bb7a6d319d9293bf68f9
Plínio Naves <pliniopjn@hotmail.com>
2018-06-05 15:07:17 -0300

    Secao 08 - DashboardHomeComponent para apresentação do módulo

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { SharedModule } from '../shared/shared.module';
+import { DashboardHomeComponent } from './components/dashboard-home/dashboard-home.component';
 
 @NgModule({
   imports: [
     SharedModule,
     DashboardRoutingModule
   ],
-  declarations: []
+  declarations: [DashboardHomeComponent]
 })
 export class DashboardModule { }
