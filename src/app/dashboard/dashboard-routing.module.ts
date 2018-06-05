ae6ad8967e8e1de00e50d0faf3a3e317a335f920
Plínio Naves <pliniopjn@hotmail.com>
2018-06-05 15:23:52 -0300

    Secao 08 - Protegendo a rota com AuthGuard no CanActivate

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { AuthGuard } from '../login/auth.guard';
 import { DashboardHomeComponent } from './components/dashboard-home/dashboard-home.component';
 
 const routes: Routes = [
-  { path: '', component: DashboardHomeComponent }
+  { path: '', component: DashboardHomeComponent, canActivate: [ AuthGuard ] }
 ];
 
 @NgModule({
