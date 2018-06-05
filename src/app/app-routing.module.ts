f2ac54f2c9d95e23419c1e03ca2ea4466a1f02dd
Plínio Naves <pliniopjn@hotmail.com>
2018-06-05 20:58:56 -0300

    Secao 08 - Carregando DashboardModule sob demanda com Lazy Loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { AuthGuard } from './login/auth.guard';
+
 const routes: Routes = [
+  {
+    path: 'dashboard',
+    loadChildren: './dashboard/dashboard.module#DashboardModule',
+    canLoad: [ AuthGuard ]
+  },
   { path: '', redirectTo: 'login', pathMatch: 'full' }
 ];
 
