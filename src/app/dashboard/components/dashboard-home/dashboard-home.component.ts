84e1e7071250b127893945b44c0e5b0f2b0650cd
Plínio Naves <pliniopjn@hotmail.com>
2018-06-06 12:44:16 -0300

    Secao 08 - Content projection e Output Event no DashboardResources

--- src/app/dashboard/components/dashboard-home/dashboard-home.component.ts
+++ src/app/dashboard/components/dashboard-home/dashboard-home.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { MatSidenav } from '@angular/material';
+
+import { AuthService } from '../../../core/services/auth.service';
 
 @Component({
   selector: 'app-dashboard-home',
   templateUrl: './dashboard-home.component.html',
   styleUrls: ['./dashboard-home.component.scss']
 })
-export class DashboardHomeComponent implements OnInit {
+export class DashboardHomeComponent {
 
-  constructor() { }
+  constructor(
+    private authService: AuthService
+  ) { }
 
-  ngOnInit() {
+  onLogout(sidenav: MatSidenav): void {
+    sidenav.close()
+      .then(() => this.authService.logout());
   }
 
 }
--- src/app/dashboard/components/dashboard-resources/dashboard-resources.component.ts
+++ src/app/dashboard/components/dashboard-resources/dashboard-resources.component.ts
@@ -1,26 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard-resources',
   template: `
     <mat-nav-list>
 
-      <a mat-list-item [routerLink]="link.url" *ngFor="let link of resources">
-        <mat-icon matListIcon>{{ link.icon }}</mat-icon>
-        <h3 matLine>{{ link.title }}</h3>
+      <a
+        mat-list-item
+        [routerLink]="link.url"
+        *ngFor="let link of resources"
+        (click)="onClose()">
+          <mat-icon matListIcon>{{ link.icon }}</mat-icon>
+          <h3 matLine>{{ link.title }}</h3>
       </a>
+
+      <ng-content></ng-content>
 
     </mat-nav-list>
   `
 })
-export class DashboardResourcesComponent {
+export class DashboardResourcesComponent implements OnInit {
+
+  @Input() isMenu = false;
+  @Output() close = new EventEmitter<void>();
 
   resources: any[] = [
     {
-      url: '/dashboard',
-      icon: 'home',
-      title: 'Home'
+      url: '/dashboard/chat',
+      icon: 'chat_bubble',
+      title: 'My Chats'
     }
   ];
+
+  ngOnInit(): void {
+    if (this.isMenu) {
+      this.resources.unshift({
+        url: '/dashboard',
+        icon: 'home',
+        title: 'Home'
+      });
+    }
+  }
+
+  onClose(): void {
+    this.close.emit();
+  }
 
 }
