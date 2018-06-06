5344286d24836846c6b4acb3ddb4c9e74cc7998c
Plínio Naves <pliniopjn@hotmail.com>
2018-06-06 12:03:16 -0300

    Secao 08 - Configurando rota filha para DashboardResources

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -3,9 +3,18 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { AuthGuard } from '../login/auth.guard';
 import { DashboardHomeComponent } from './components/dashboard-home/dashboard-home.component';
+import { DashboardResourcesComponent } from './components/dashboard-resources/dashboard-resources.component';
 
 const routes: Routes = [
-  { path: '', component: DashboardHomeComponent, canActivate: [ AuthGuard ] }
+  {
+    path: '',
+    component: DashboardHomeComponent,
+    canActivate: [ AuthGuard ],
+    canActivateChild: [ AuthGuard ],
+    children: [
+      { path: '', component: DashboardResourcesComponent }
+    ]
+  }
 ];
 
 @NgModule({
