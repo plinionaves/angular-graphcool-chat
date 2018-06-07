9d667b7831da62cef6bd30ca2759a01902236355
Plínio Naves <pliniopjn@hotmail.com>
2018-06-07 16:02:43 -0300

    Secao 09 - Método para buscar usuários cadastrados com Query allUsers

---
+++ src/app/core/services/user.graphql.ts
@@ -0,0 +1,18 @@
+import gql from 'graphql-tag';
+import { User } from '../models/user.model';
+
+export interface AllUsersQuery {
+  allUsers: User[];
+}
+
+export const ALL_USERS_QUERY = gql`
+  query AllUsersQuery {
+    allUsers(
+      orderBy: name_ASC
+    ) {
+      id
+      name
+      email
+    }
+  }
+`;
--- src/app/core/services/user.service.ts
+++ src/app/core/services/user.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { Apollo } from 'apollo-angular';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { User } from '../models/user.model';
+import { ALL_USERS_QUERY, AllUsersQuery } from './user.graphql';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(
+    private apollo: Apollo
+  ) { }
+
+  allUsers(): Observable<User[]> {
+    return this.apollo
+      .query<AllUsersQuery>({
+        query: ALL_USERS_QUERY
+      }).pipe(
+        map(res => res.data.allUsers)
+      );
+  }
+
 }
