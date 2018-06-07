4dc8c0a7bab3a9c3acf773c542e5dcecd7c1522e
Plínio Naves <pliniopjn@hotmail.com>
2018-06-07 09:26:12 -0300

    Secao 09 - ChatListComponent para exibir lista de Chats

--- src/app/chat/chat.module.ts
+++ src/app/chat/chat.module.ts
@@ -4,12 +4,13 @@ import { ChatRoutingModule } from './chat-routing.module';
 import { SharedModule } from '../shared/shared.module';
 import { ChatTabComponent } from './components/chat-tab/chat-tab.component';
 import { ChatUsersComponent } from './components/chat-users/chat-users.component';
+import { ChatListComponent } from './components/chat-list/chat-list.component';
 
 @NgModule({
   imports: [
     SharedModule,
     ChatRoutingModule
   ],
-  declarations: [ChatTabComponent, ChatUsersComponent]
+  declarations: [ChatTabComponent, ChatUsersComponent, ChatListComponent]
 })
 export class ChatModule { }
---
+++ src/app/chat/components/chat-list/chat-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chat-list',
+  templateUrl: './chat-list.component.html',
+  styleUrls: ['./chat-list.component.scss']
+})
+export class ChatListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
