6eff9cfb9b87159c7d52d1b2c34ae9ad0eadc77e
Plínio Naves <pliniopjn@hotmail.com>
2018-06-08 17:41:15 -0300

    Secao 10 - ChatWindowComponent para troca de mensagens

--- src/app/chat/chat.module.ts
+++ src/app/chat/chat.module.ts
@@ -5,12 +5,13 @@ import { SharedModule } from '../shared/shared.module';
 import { ChatTabComponent } from './components/chat-tab/chat-tab.component';
 import { ChatUsersComponent } from './components/chat-users/chat-users.component';
 import { ChatListComponent } from './components/chat-list/chat-list.component';
+import { ChatWindowComponent } from './components/chat-window/chat-window.component';
 
 @NgModule({
   imports: [
     SharedModule,
     ChatRoutingModule
   ],
-  declarations: [ChatTabComponent, ChatUsersComponent, ChatListComponent]
+  declarations: [ChatTabComponent, ChatUsersComponent, ChatListComponent, ChatWindowComponent]
 })
 export class ChatModule { }
---
+++ src/app/chat/components/chat-window/chat-window.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chat-window',
+  templateUrl: './chat-window.component.html',
+  styleUrls: ['./chat-window.component.scss']
+})
+export class ChatWindowComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
