5e19c758b29544690d340c7a4ba965d8781b456f
Plínio Naves <pliniopjn@hotmail.com>
2018-06-11 09:25:04 -0300

    Secao 10 - Criando ChatModel e MessageModel

---
+++ src/app/chat/models/chat.model.ts
@@ -0,0 +1,13 @@
+import { Message } from './message.model';
+import { User } from '../../core/models/user.model';
+
+export class Chat {
+
+  id?: string;
+  createdAt?: string;
+  isGroup?: boolean;
+  title?: string;
+  users?: User[];
+  messages?: Message[];
+
+}
---
+++ src/app/chat/models/message.model.ts
@@ -0,0 +1,10 @@
+import { Chat } from './chat.model';
+import { User } from '../../core/models/user.model';
+
+export interface Message {
+  id?: string;
+  text?: string;
+  createdAt?: string;
+  sender?: User;
+  chat?: Chat;
+}
