17cc991d5803c7268c1a2e278b2e9a014dd86899
Plínio Naves <pliniopjn@hotmail.com>
2018-06-11 16:31:20 -0300

    Secao 10 - ChatService: buscando Chat por id ou por usuários na mesma requisição

--- src/app/chat/services/chat.service.ts
+++ src/app/chat/services/chat.service.ts
@@ -3,7 +3,7 @@ import { Apollo } from 'apollo-angular';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
-import { AllChatsQuery, USER_CHATS_QUERY } from './chat.graphql';
+import { AllChatsQuery, ChatQuery, CHAT_BY_ID_OR_BY_USERS_QUERY, USER_CHATS_QUERY } from './chat.graphql';
 import { AuthService } from '../../core/services/auth.service';
 import { Chat } from '../models/chat.model';
 
@@ -25,6 +25,19 @@ export class ChatService {
       }
     }).pipe(
       map(res => res.data.allChats)
+    );
+  }
+
+  getChatByIdOrByUsers(chatOrUserId: string): Observable<Chat> {
+    return this.apollo.query<ChatQuery | AllChatsQuery>({
+      query: CHAT_BY_ID_OR_BY_USERS_QUERY,
+      variables: {
+        chatId: chatOrUserId,
+        loggedUserId: this.authService.authUser.id,
+        targetUserId: chatOrUserId
+      }
+    }).pipe(
+      map(res => (res.data['Chat']) ? res.data['Chat'] : res.data['allChats'][0])
     );
   }
 
