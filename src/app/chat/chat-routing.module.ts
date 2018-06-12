807982c19eed54af40e1b90d367e57516771d6fa
Plínio Naves <pliniopjn@hotmail.com>
2018-06-11 21:40:38 -0300

    Secao 10 - Configurando Resolve Guard na rota

--- src/app/chat/chat-routing.module.ts
+++ src/app/chat/chat-routing.module.ts
@@ -6,6 +6,7 @@ import { ChatListComponent } from './components/chat-list/chat-list.component';
 import { ChatTabComponent } from './components/chat-tab/chat-tab.component';
 import { ChatUsersComponent } from './components/chat-users/chat-users.component';
 import { ChatWindowComponent } from './components/chat-window/chat-window.component';
+import { ChatWindowResolver } from './components/chat-window/chat-window.resolver';
 
 const routes: Routes = [
   {
@@ -18,11 +19,17 @@ const routes: Routes = [
       { path: '', component: ChatListComponent }
     ]
   },
-  { path: ':id', component: ChatWindowComponent, canActivate: [ AuthGuard ] }
+  {
+    path: ':id',
+    component: ChatWindowComponent,
+    canActivate: [ AuthGuard ],
+    resolve: { chat: ChatWindowResolver }
+  }
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [ ChatWindowResolver ]
 })
 export class ChatRoutingModule { }
