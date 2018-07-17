8ff3ed651d00f92c0880789cc29e4c5010c66c06
Plínio Naves <pliniopjn@hotmail.com>
2018-07-17 18:06:56 -0300

    Secao 10 - ChatMessageComponent para cada mensagem com Angular CLI

--- src/app/chat/chat.module.ts
+++ src/app/chat/chat.module.ts
@@ -1,17 +1,24 @@
 import { NgModule } from '@angular/core';
 
+import { ChatListComponent } from './components/chat-list/chat-list.component';
+import { ChatMessageComponent } from './components/chat-message/chat-message.component';
 import { ChatRoutingModule } from './chat-routing.module';
-import { SharedModule } from '../shared/shared.module';
 import { ChatTabComponent } from './components/chat-tab/chat-tab.component';
 import { ChatUsersComponent } from './components/chat-users/chat-users.component';
-import { ChatListComponent } from './components/chat-list/chat-list.component';
 import { ChatWindowComponent } from './components/chat-window/chat-window.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   imports: [
     SharedModule,
     ChatRoutingModule
   ],
-  declarations: [ChatTabComponent, ChatUsersComponent, ChatListComponent, ChatWindowComponent]
+  declarations: [
+    ChatListComponent,
+    ChatMessageComponent,
+    ChatTabComponent,
+    ChatUsersComponent,
+    ChatWindowComponent
+  ]
 })
 export class ChatModule { }
