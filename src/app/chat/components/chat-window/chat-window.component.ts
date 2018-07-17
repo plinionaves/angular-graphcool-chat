73f34608168fd1cb21af10fd75ad49fbc68620b1
Plínio Naves <pliniopjn@hotmail.com>
2018-07-17 15:24:27 -0300

    Secao 10 - Criando novo Chat privado ao enviar nova mensagem

--- src/app/chat/components/chat-window/chat-window.component.ts
+++ src/app/chat/components/chat-window/chat-window.component.ts
@@ -6,6 +6,7 @@ import { map, mergeMap, tap, take } from 'rxjs/operators';
 
 import { AuthService } from '../../../core/services/auth.service';
 import { Chat } from '../../models/chat.model';
+import { ChatService } from '../../services/chat.service';
 import { Message } from '../../models/message.model';
 import { MessageService } from '../../services/message.service';
 import { User } from '../../../core/models/user.model';
@@ -26,6 +27,7 @@ export class ChatWindowComponent implements OnDestroy, OnInit {
 
   constructor(
     private authService: AuthService,
+    private chatService: ChatService,
     private messageService: MessageService,
     private route: ActivatedRoute,
     private title: Title,
@@ -59,13 +61,33 @@ export class ChatWindowComponent implements OnDestroy, OnInit {
   sendMessage(): void {
     this.newMessage = this.newMessage.trim();
     if (this.newMessage) {
-      this.messageService.createMessage({
-        text: this.newMessage,
-        chatId: this.chat.id,
-        senderId: this.authService.authUser.id
-      }).subscribe(console.log);
-      this.newMessage = '';
+
+      if (this.chat) {
+
+        this.messageService.createMessage({
+          text: this.newMessage,
+          chatId: this.chat.id,
+          senderId: this.authService.authUser.id
+        }).pipe(take(1)).subscribe(console.log);
+
+        this.newMessage = '';
+
+      } else {
+        this.createPrivateChat();
+      }
+
     }
+  }
+
+  private createPrivateChat(): void {
+    this.chatService.createPrivateChat(this.recipientId)
+      .pipe(
+        take(1),
+        tap((chat: Chat) => {
+          this.chat = chat;
+          this.sendMessage();
+        })
+      ).subscribe();
   }
 
   ngOnDestroy(): void {
