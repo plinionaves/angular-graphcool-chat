2b5b162fe579a47373a303baa6488b37232b7c5b
Plínio Naves <pliniopjn@hotmail.com>
2018-09-04 18:55:07 -0300

    Secao 14 - UserModule e UserRoutingModule com Angular CLI

---
+++ src/app/user/user.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { UserRoutingModule } from './user-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    UserRoutingModule
+  ],
+  declarations: []
+})
+export class UserModule { }
