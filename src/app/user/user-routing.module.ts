29d5fb401afb5ccb9a367a064fbcdaf92ac160d9
Plínio Naves <pliniopjn@hotmail.com>
2018-09-04 20:42:31 -0300

    Secao 14 - Configurando rota interna para o perfil

--- src/app/user/user-routing.module.ts
+++ src/app/user/user-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { AuthGuard } from '../login/auth.guard';
+import { UserProfileComponent } from './components/user-profile/user-profile.component';
+
+const routes: Routes = [
+  { path: '', component: UserProfileComponent, canActivate: [ AuthGuard ] }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
