b2aa12bb45575d8b2d295c567e26cf8264b1d073
Plínio Naves <pliniopjn@hotmail.com>
2018-09-05 16:00:35 -0300

    Secao 14 - Fazendo o login funcionar novamente

--- src/app/login/components/login/login.component.ts
+++ src/app/login/components/login/login.component.ts
@@ -69,9 +69,16 @@ export class LoginComponent implements OnInit, OnDestroy {
           this.authService.setRememberMe(this.loginForm.value);
           const redirect: string = this.authService.redirectUrl || '/dashboard';
           console.log('redirecting...', redirect);
-          this.router.navigate([redirect]);
-          this.authService.redirectUrl = null;
-          this.configs.isLoading = false;
+
+          this.authService.isAuthenticated
+            .pipe(takeWhile(() => this.alive))
+            .subscribe((is: boolean) => {
+              if (is) {
+                this.router.navigate([redirect]);
+                this.authService.redirectUrl = null;
+                this.configs.isLoading = false;
+              }
+            });
         },
         err => {
           console.log(err);
