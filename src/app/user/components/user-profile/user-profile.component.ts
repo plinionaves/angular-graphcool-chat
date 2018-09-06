f3ec7ad9b2e14c47fa4d23b9f823ef92a10f300a
Plínio Naves <pliniopjn@hotmail.com>
2018-09-06 14:23:15 -0300

    Secao 14 - Mensagem de sucesso ou erro com MatSnackBar

--- src/app/user/components/user-profile/user-profile.component.ts
+++ src/app/user/components/user-profile/user-profile.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material';
 import { take } from 'rxjs/operators';
 
 import { AuthService } from '../../../core/services/auth.service';
+import { ErrorService } from '../../../core/services/error.service';
 import { User } from '../../../core/models/user.model';
 import { UserService } from '../../../core/services/user.service';
 
@@ -17,6 +19,8 @@ export class UserProfileComponent implements OnInit {
 
   constructor(
     private authService: AuthService,
+    private errorService: ErrorService,
+    private snackBar: MatSnackBar,
     private userService: UserService
   ) { }
 
@@ -25,12 +29,14 @@ export class UserProfileComponent implements OnInit {
   }
 
   onSave(): void {
-    console.log('User: ', this.user);
+    let message: string;
     this.userService.updateUser(this.user)
       .pipe(take(1))
       .subscribe(
-        (user: User) => {
-          console.log('Updated! ', user);
+        (user: User) => message = 'Profile updated!',
+        error => message = this.errorService.getErrorMessage(error),
+        () => {
+          this.snackBar.open(message, 'OK', { duration: 3000, verticalPosition: 'top' });
         }
       );
   }
