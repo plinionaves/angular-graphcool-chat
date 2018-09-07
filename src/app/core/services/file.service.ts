40d0714b5023bdfcfc3dbab50e8f3a7d3f9e3f29
Plínio Naves <pliniopjn@hotmail.com>
2018-09-07 17:51:12 -0300

    Secao 15 - Método genérico para upload de arquivos no FileService

--- src/app/core/services/file.service.ts
+++ src/app/core/services/file.service.ts
@@ -1,9 +1,24 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { FileModel } from '../models/file.model';
+import { GRAPHCOOL_CONFIG, GraphcoolConfig } from '../providers/graphcool-config.provider';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FileService {
 
-  constructor() { }
+  constructor(
+    @Inject(GRAPHCOOL_CONFIG) private graphcoolConfig: GraphcoolConfig,
+    private http: HttpClient
+  ) { }
+
+  upload(file: File): Observable<FileModel> {
+    const formData = new FormData();
+    formData.append('data', file);
+    return this.http.post<FileModel>(this.graphcoolConfig.fileAPI, formData);
+  }
+
 }
