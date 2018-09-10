891a073a32bf19fea8363bf9d313533af4195587
Plínio Naves <pliniopjn@hotmail.com>
2018-09-10 14:37:49 -0300

    Secao 15 - ImagePreviewComponent: estilizando com CSS

--- src/app/shared/components/image-preview/image-preview.component.ts
+++ src/app/shared/components/image-preview/image-preview.component.ts
@@ -3,7 +3,23 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-image-preview',
   templateUrl: './image-preview.component.html',
-  styles: []
+  styles: [
+    `
+      img {
+        max-width: 100%;
+        margin: auto;
+        display: block;
+      }
+
+      img[mat-card-image] {
+        margin-top: 0;
+      }
+
+      mat-card {
+        padding: 0;
+      }
+    `
+  ]
 })
 export class ImagePreviewComponent implements OnInit {
 
