243dbc1ad497e6c3854bb5a6d8587754296b9734
Plínio Naves <pliniopjn@hotmail.com>
2018-09-11 20:39:05 -0300

    Secao 15 - Implementando o construtor e novo método no ChatModel

--- src/app/chat/models/chat.model.ts
+++ src/app/chat/models/chat.model.ts
@@ -1,5 +1,8 @@
+import { FileModel } from '../../core/models/file.model';
 import { Message } from './message.model';
 import { User } from '../../core/models/user.model';
+
+import { graphcoolConfig } from './../../core/providers/graphcool-config.provider';
 
 export class Chat {
 
@@ -9,5 +12,21 @@ export class Chat {
   title?: string;
   users?: User[];
   messages?: Message[];
+  photo?: FileModel;
+
+  constructor(chat: Chat) {
+    Object.keys(chat)
+      .forEach(key => this[key] = chat[key]);
+  }
+
+  getPhotoURL?(): string {
+    if (this.photo && this.photo.secret) {
+      return `${graphcoolConfig.fileDownloadURL}/${this.photo.secret}`;
+    }
+    if (this.isGroup) {
+      return 'assets/images/group-no-photo.png';
+    }
+    return this.users[0].getPhotoURL() || 'assets/images/user-no-photo.png';
+  }
 
 }
